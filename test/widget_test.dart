4a13022c82f4ab9fa8c0f1c85487f2b8f1528bdb
RousanChandra <syahrousan@gmail.com>
2025-11-19 21:28:17 +0700

    tutor9

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:football_news_mobile/main.dart';
+void main() {
+  testWidgets('dummy smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: Center(child: Text('0')),
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+    expect(find.text('0'), findsOneWidget);
+  });
+}
 
-void main() {}
